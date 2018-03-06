28b320b6f2d70b92378a6b721b07cc866295877f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2018-03-06 16:26:58 +0100

    Remove test object.

--- src/main/scala/amounts/Recipe.scala
+++ src/main/scala/amounts/Recipe.scala
@@ -2,16 +2,10 @@ package amounts
 
 import algebra.ring.AdditiveMonoid
 import base.FunctionalAnyPrefix.Implicits._
-import base.Mineral.Iron
-import base.Trace.Tryptophane
-import base.Vitamin.VitaminC
 import base._
 import physical.NamedUnitAnyPrefix.Implicits._
 import physical.PUnit.Syntax._
 import physical.PhysicalAmount.Implicits._
-import physical.Prefix.Syntax._
-import physical._
-import spire.algebra.AdditiveSemigroup
 import spire.implicits._
 import spire.math.Numeric
 
@@ -48,59 +42,3 @@ object Recipe {
   }
 }
 
-object TestMe {
-
-  def main(args: Array[String]): Unit = {
-
-    val pa = PhysicalAmount.fromRelative[Floating, Single](100)(Numeric[Floating], Single)
-    val base: Mass[Floating, Single] = Mass(pa)
-
-    val ingredient1 = new Ingredient[Floating] {
-      override val basePalette: Palette[Floating] = new Functional[Mass[Floating, _], Nutrient]({
-        case Iron => Mass(PhysicalAmount.fromRelative[Floating, Milli](5)) //=> 11.75 milligram 0.01175
-        case VitaminC => Mass(PhysicalAmount.fromRelative[Floating, Single](3))
-        case Tryptophane => Mass(PhysicalAmount.fromRelative[Floating, Micro](17))
-      }
-      )
-
-      override val baseReference: Mass[Floating, _] = base
-
-      override def weightPerMillilitre[P: Prefix]: Mass[Floating, P] =
-        Mass(PhysicalAmount.fromAbsolute[Floating, P](0.470))
-    }
-
-    val ingredient2 = new Ingredient[Floating] {
-      override val basePalette: Palette[Floating] = new Functional[Mass[Floating, _], Nutrient]({
-        case Iron => Mass(PhysicalAmount.fromRelative[Floating, Micro](16)) //=> 48 microgram 0.000048
-        case VitaminC => Mass(PhysicalAmount.fromRelative[Floating, Milli](1))
-        case Tryptophane => Mass(PhysicalAmount.fromRelative[Floating, Nano](3))
-      }
-      )
-
-      override val baseReference: Mass[Floating, _] = base
-
-      override def weightPerMillilitre[P: Prefix]: Mass[Floating, P] =
-        Mass(PhysicalAmount.fromAbsolute[Floating, P](1.3))
-    }
-
-    val ingredients = List(
-      FixedVolume[Floating, Milli](FixedVolume.Cup[Floating](), ingredient1, 2),
-      Metric(Mass(PhysicalAmount.fromRelative[Floating, Single](300)), ingredient2)
-    )
-
-    val recipe = Recipe(ingredients)
-    val pal = recipe.palette
-
-    println(
-      s"iron = ${pal(Iron)}, vitaminC = ${pal(VitaminC)}, tryptophane = ${pal(Tryptophane)}")
-
-    val nu1: NamedUnit[Floating, _, Gram] = Mass(PhysicalAmount.fromRelative[Floating, Milli](100))
-    val nu2: NamedUnit[Floating, _, Gram] = Mass(PhysicalAmount.fromRelative[Floating, Nano](5))
-
-    import NamedUnitAnyPrefix.Implicits._
-
-
-    implicit val asg: AdditiveSemigroup[NamedUnit[Floating, _, Gram]] = NamedUnitAnyPrefix.Implicits.additiveSemigroupNUAP
-    println(asg.plus(nu1, nu2))
-  }
-}
