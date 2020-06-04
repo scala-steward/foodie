fd3cf6051ff12258523d0eafc819fcd0c90ac601
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-05 01:14:58 +0200

    Use implicit ring parameter

--- app/computation/base/Functional.scala
+++ app/computation/base/Functional.scala
@@ -3,7 +3,6 @@ package computation.base
 import algebra.ring.AdditiveMonoid
 import spire.algebra._
 import spire.syntax.vectorSpace._
-import computation.base.math.LeftModuleUtil.Implicits._
 
 case class Functional[N, A](f: A => N) extends (A => N) {
   override def apply(v1: A): N = f(v1)
@@ -18,16 +17,17 @@ object Functional {
 
   object Implicits {
 
-    implicit def functionalLeftModule[F, A](implicit leftModule: LeftModule[F, F]): LeftModule[Functional[F, A], F] = new LeftModule[Functional[F, A], F] {
-      override implicit def scalar: Ring[F] = Ring[F]
+    implicit def functionalLeftModule[V, L, A](implicit leftModule: LeftModule[V, L],
+                                               ringL: Ring[L]): LeftModule[Functional[V, A], L] = new LeftModule[Functional[V, A], L] {
+      override def scalar: Ring[L] = ringL
 
-      override def timesl(r: F, v: Functional[F, A]): Functional[F, A] = Functional(a => r * v(a))
+      override def timesl(r: L, v: Functional[V, A]): Functional[V, A] = Functional(a => r *: v(a))
 
-      override def negate(x: Functional[F, A]): Functional[F, A] = Functional(a => -x(a))
+      override def negate(x: Functional[V, A]): Functional[V, A] = Functional(a => -x(a))
 
-      override def zero: Functional[F, A] = Functional(_ => scalar.zero)
+      override def zero: Functional[V, A] = Functional(_ => leftModule.zero)
 
-      override def plus(x: Functional[F, A], y: Functional[F, A]): Functional[F, A] = Functional(a => x(a) + y(a))
+      override def plus(x: Functional[V, A], y: Functional[V, A]): Functional[V, A] = Functional(a => x(a) + y(a))
     }
 
   }
