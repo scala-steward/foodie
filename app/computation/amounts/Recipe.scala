becdb62148b56959919f8606d28ac5ff563d187d
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-05 18:48:46 +0200

    Simplify recipes

--- app/computation/amounts/Recipe.scala
+++ app/computation/amounts/Recipe.scala
@@ -1,45 +1,41 @@
-package amounts
+package computation.amounts
 
-import base._
-import base.math.ScalarMultiplication.Syntax._
-import physical.NamedUnitAnyPrefix.Implicits._
-import physical.PhysicalAmount.Implicits._
-import Functional.Implicits._
-import physical.NamedUnit.Implicits._
-import spire.math.Numeric
-import Palette.Implicits._
+import algebra.ring.AdditiveMonoid
+import computation.amounts.Palette.Implicits._
+import computation.base.math.LeftModuleUtil.Implicits._
+import computation.physical.NamedUnit.Implicits._
+import spire.algebra.Field
+import spire.syntax.leftModule._
 
 /**
-  * The main type representing a recipe.
-  * Put simply, a recipe is merely a list of amounts (of ingredients).
-  * This simplification allows a very simple computation of the overall nutritional palette,
-  * which is essentially the sum of the individual palettes of the amounts.
-  *
-  * @param ingredients The contents that are put in the recipe.
-  *                    For simplicity, it is assumed that no nutrients are lost in the cooking process.
-  * @tparam N The type of number in which the amounts are denoted (usually [[base.Floating]].
-  */
-case class Recipe[N: Numeric](ingredients: Traversable[AmountOf[N]]) {
+ * The main type representing a recipe.
+ * Put simply, a recipe is merely a list of amounts (of ingredients).
+ * This simplification allows a very simple computation of the overall nutritional palette,
+ * which is essentially the sum of the individual palettes of the amounts.
+ *
+ * @param ingredients The contents that are put in the recipe.
+ *                    For simplicity, it is assumed that no nutrients are lost in the cooking process.
+ */
+case class Recipe[N: Field](ingredients: Iterable[AmountOf[N]]) {
   /**
-    * The overall nutritional palette associated with this recipe.
-    */
+   * The overall nutritional palette associated with this recipe.
+   */
   val palette: Palette[N] = Recipe.palette(ingredients)
 
-  def scale(factor: N): Palette[N] = palette.scale(factor)
+  def scale(factor: N): Palette[N] = factor *: palette
 }
 
 object Recipe {
 
   /**
-    * Compute the sum of individual palettes from a list of ingredients.
-    *
-    * @param ingredients The ingredients of a recipe.
-    * @tparam N The numeric type in which the amounts are given.
-    * @return The nutritional content of the overall dish.
-    */
-  def palette[N: Numeric](ingredients: Traversable[AmountOf[N]]): Palette[N] = {
-    val palettes: Traversable[Palette[N]] = ingredients.map(AmountOf.palette[N])
-    val resultPalette = CollectionUtil.sum(palettes)
+   * Compute the sum of individual palettes from a list of ingredients.
+   *
+   * @param ingredients The ingredients of a recipe.
+   * @return The nutritional content of the overall dish.
+   */
+  def palette[N: Field](ingredients: Iterable[AmountOf[N]]): Palette[N] = {
+    val palettes: Iterable[Palette[N]] = ingredients.map(AmountOf.palette[N])
+    val resultPalette = AdditiveMonoid[Palette[N]].sum(palettes)
     resultPalette
   }
 }
