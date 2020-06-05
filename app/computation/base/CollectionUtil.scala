0162177c2e7350bd138d3c7230b323a64065f47c
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-05 18:43:52 +0200

    Remove auxiliary function

--- app/computation/base/CollectionUtil.scala
+++ app/computation/base/CollectionUtil.scala
@@ -1,10 +1,7 @@
-package base
-
-import algebra.ring.AdditiveMonoid
-import spire.implicits._
+package computation.base
 
 object CollectionUtil {
 
-  def sum[A: AdditiveMonoid](xs: Traversable[A]): A = xs.foldLeft(AdditiveMonoid[A].zero)(_ + _)
+  // TODO: consider removing
 
 }
