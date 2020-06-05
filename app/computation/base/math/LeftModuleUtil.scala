84b5ed2247e17bb4bdc3317ac32d39e53641e426
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-05 18:42:24 +0200

    Add utility construction

--- app/computation/base/math/LeftModuleUtil.scala
+++ app/computation/base/math/LeftModuleUtil.scala
@@ -1,6 +1,7 @@
 package computation.base.math
 
 import spire.algebra.{LeftModule, Ring}
+import spire.syntax.ring._
 
 object LeftModuleUtil {
 
@@ -21,6 +22,18 @@ object LeftModuleUtil {
       override def plus(x: L, y: L): L = leftModule.scalar.plus(x, y)
 
       override def times(x: L, y: L): L = leftModule.scalar.times(x, y)
+    }
+
+    implicit def ringAsLeftModule[R](implicit ring: Ring[R]): LeftModuleSelf[R] = new LeftModuleSelf[R] {
+      override def scalar: Ring[R] = ring
+
+      override def timesl(r: R, v: R): R = r * v
+
+      override def negate(x: R): R = -x
+
+      override def zero: R = ring.zero
+
+      override def plus(x: R, y: R): R = x + y
     }
   }
 
