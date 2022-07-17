d7938597cdeceeba4e8942bb5a9fa4350b906da2
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-07-17 15:51:19 +0200

    Add transformation from DB

--- app/services/recipe/Recipe.scala
+++ app/services/recipe/Recipe.scala
@@ -2,7 +2,9 @@ package services.recipe
 
 import db.generated.Tables
 import io.scalaland.chimney.Transformer
+import io.scalaland.chimney.dsl.TransformerOps
 import shapeless.tag.@@
+import utils.IdUtils.Implicits._
 
 import java.util.UUID
 
@@ -10,7 +12,23 @@ case class Recipe(
     id: UUID @@ RecipeId,
     name: String,
     description: Option[String],
-    ingredients: Vector[Ingredient]
+    ingredients: Seq[Ingredient]
 )
 
-object Recipe {}
+object Recipe {
+
+  case class DBRepresentation(
+      recipeRow: Tables.RecipeRow,
+      ingredientRows: Seq[Tables.RecipeIngredientRow]
+  )
+
+  implicit val fromDB: Transformer[DBRepresentation, Recipe] =
+    Transformer
+      .define[DBRepresentation, Recipe]
+      .withFieldComputed(_.id, _.recipeRow.id.transformInto[UUID @@ RecipeId])
+      .withFieldComputed(_.name, _.recipeRow.name)
+      .withFieldComputed(_.description, _.recipeRow.description)
+      .withFieldComputed(_.ingredients, _.ingredientRows.map(_.transformInto[Ingredient]))
+      .buildTransformer
+
+}
