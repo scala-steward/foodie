0fc3e472b17869aa59e81128b244ea3c6cbfbc8e
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-07-19 23:20:45 +0200

    Adjust JSON meal structure

--- app/controllers/meal/Meal.scala
+++ app/controllers/meal/Meal.scala
@@ -2,12 +2,14 @@ package controllers.meal
 
 import io.circe.generic.JsonCodec
 
-import java.time.Instant
+import java.time.{ LocalDate, LocalTime }
 import java.util.UUID
 
 @JsonCodec
 case class Meal(
     id: UUID,
-    date: Instant,
+    date: LocalDate,
+    time: LocalTime,
+    name: Option[String],
     entries: Seq[MealEntry]
 )
