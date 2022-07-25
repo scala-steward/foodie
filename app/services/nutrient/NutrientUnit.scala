8d1bc49d0881181cc0c9783ac27eb089537f705f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-07-25 22:47:21 +0200

    Add nutrient unit type

---
+++ app/services/nutrient/NutrientUnit.scala
@@ -0,0 +1,35 @@
+package services.nutrient
+
+import enumeratum.{ EnumEntry, Enum }
+
+sealed trait NutrientUnit extends EnumEntry
+
+object NutrientUnit extends Enum[NutrientUnit] {
+
+  case object Kilocalorie extends NutrientUnit {
+    override val entryName: String = "kCal"
+  }
+
+  case object Kilojoule extends NutrientUnit {
+    override val entryName: String = "kJ"
+  }
+
+  case object Microgram extends NutrientUnit {
+    override val entryName: String = "µg"
+  }
+
+  case object Milligram extends NutrientUnit {
+    override val entryName: String = "mg"
+  }
+
+  case object Gram extends NutrientUnit {
+    override val entryName: String = "g"
+  }
+
+  case object NiacinEquivalent extends NutrientUnit {
+    override val entryName: String = "NE"
+  }
+
+  override lazy val values: IndexedSeq[NutrientUnit] = findValues
+
+}
