9517dcf81ff2a5bd74b7f5944dc08e44fb2f8660
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-07-27 00:23:14 +0200

    Implement stats service

--- app/services/stats/StatsService.scala
+++ app/services/stats/StatsService.scala
@@ -1,21 +1,88 @@
 package services.stats
 
+import cats.syntax.traverse._
+import db.generated.Tables
+import io.scalaland.chimney.dsl.TransformerOps
+import play.api.db.slick.{ DatabaseConfigProvider, HasDatabaseConfigProvider }
+import services.{ MealId, RecipeId, UserId }
+import services.meal.MealService
+import services.nutrient.{ NutrientMap, NutrientService }
+import services.recipe.RecipeService
+import slick.dbio.DBIO
+
 import javax.inject.Inject
-import scala.concurrent.Future
+import scala.concurrent.{ ExecutionContext, Future }
+import slick.jdbc.PostgresProfile
+import slick.jdbc.PostgresProfile.api._
+import utils.DBIOUtil
+import utils.DBIOUtil.instances._
+import spire.implicits._
+import utils.TransformerUtils.Implicits._
 
 trait StatsService {
 
-  def nutrientsOverTime(requestInterval: RequestInterval)
+  def nutrientsOverTime(userId: UserId, requestInterval: RequestInterval): Future[Stats]
 
 }
 
 object StatsService {
 
-  class Live @Inject() (companion: Companion) extends StatsService {
-    override def nutrientsOverTime(requestInterval: RequestInterval) = ???
+  class Live @Inject() (
+      override protected val dbConfigProvider: DatabaseConfigProvider,
+      companion: Companion
+  )(implicit
+      ec: ExecutionContext
+  ) extends StatsService
+      with HasDatabaseConfigProvider[PostgresProfile] {
+
+    override def nutrientsOverTime(userId: UserId, requestInterval: RequestInterval): Future[Stats] =
+      db.run(companion.nutrientsOverTime(userId, requestInterval))
+
   }
 
-  trait Companion
+  trait Companion {
+    def nutrientsOverTime(userId: UserId, requestInterval: RequestInterval)(implicit ec: ExecutionContext): DBIO[Stats]
+  }
 
-  object Live extends Companion
+  object Live extends Companion {
+
+    override def nutrientsOverTime(
+        userId: UserId,
+        requestInterval: RequestInterval
+    )(implicit
+        ec: ExecutionContext
+    ): DBIO[Stats] = {
+      val dateFilter = DBIOUtil.dateFilter(requestInterval.from, requestInterval.to)
+      for {
+        mealIdsPlain <- Tables.Meal.filter(m => dateFilter(m.consumedOnDate)).map(_.id).result
+        mealIds = mealIdsPlain.map(_.transformInto[MealId])
+        meals <- mealIds.traverse(MealService.Live.getMeal(userId, _)).map(_.flatten)
+        recipes <-
+          meals
+            .flatMap(_.entries.map(_.recipeId))
+            .distinct
+            .traverse(RecipeService.Live.getRecipe(userId, _))
+            .map(_.flatten)
+        nutrientsPerRecipe <-
+          recipes
+            .traverse(r =>
+              NutrientService.Live
+                .nutrientsOfIngredients(r.ingredients)
+                .map(r.id -> _): DBIO[(RecipeId, NutrientMap)]
+            )
+            .map(_.toMap)
+      } yield {
+        val nutrientMap = meals
+          .flatMap(_.entries)
+          .map(me => me.factor *: nutrientsPerRecipe(me.recipeId))
+          .qsum
+        Stats(
+          meals = meals,
+          nutrientMap = nutrientMap
+        )
+      }
+    }
+
+  }
+
 }
