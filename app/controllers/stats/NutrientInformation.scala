235ea601b8b8f9adf7d150b68d3ef83925eb0081
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-07-27 21:57:35 +0200

    Extend types for stats

--- app/controllers/stats/NutrientInformation.scala
+++ app/controllers/stats/NutrientInformation.scala
@@ -1,7 +1,10 @@
 package controllers.stats
 
+import io.circe.generic.JsonCodec
+
+@JsonCodec
 case class NutrientInformation(
     name: String,
     unit: NutrientUnit,
-    amount: BigDecimal
+    amounts: Amounts
 )
---
+++ app/controllers/stats/NutrientUnit.scala
@@ -0,0 +1,45 @@
+package controllers.stats
+
+import enumeratum.{ CirceEnum, Enum, EnumEntry }
+import io.scalaland.chimney.Transformer
+
+sealed trait NutrientUnit extends EnumEntry
+
+object NutrientUnit extends Enum[NutrientUnit] with CirceEnum[NutrientUnit] {
+
+  case object Kilocalorie extends NutrientUnit {
+    override val entryName: String = "kCal"
+  }
+
+  case object Kilojoule extends NutrientUnit {
+    override val entryName: String = "kJ"
+  }
+
+  case object Microgram extends NutrientUnit {
+    override val entryName: String = "µg"
+  }
+
+  case object Milligram extends NutrientUnit {
+    override val entryName: String = "mg"
+  }
+
+  case object Gram extends NutrientUnit {
+    override val entryName: String = "g"
+  }
+
+  case object NiacinEquivalent extends NutrientUnit {
+    override val entryName: String = "NE"
+  }
+
+  override lazy val values: IndexedSeq[NutrientUnit] = findValues
+
+  implicit val fromDomain: Transformer[services.nutrient.NutrientUnit, NutrientUnit] = {
+    case services.nutrient.NutrientUnit.Kilocalorie      => Kilocalorie
+    case services.nutrient.NutrientUnit.Kilojoule        => Kilojoule
+    case services.nutrient.NutrientUnit.Microgram        => Microgram
+    case services.nutrient.NutrientUnit.Milligram        => Milligram
+    case services.nutrient.NutrientUnit.Gram             => Gram
+    case services.nutrient.NutrientUnit.NiacinEquivalent => NiacinEquivalent
+  }
+
+}
