3635418f28613af3aa2b2b5c2d283f5f6b11f3a4
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-11-08 21:49:48 +0100

    Use Dict.Extra to avoid generic code

--- frontend/src/Pages/Util/DictUtil.elm
+++ frontend/src/Pages/Util/DictUtil.elm
@@ -1,21 +1,19 @@
 module Pages.Util.DictUtil exposing (..)
 
 import Dict exposing (Dict)
+import Dict.Extra
 import Maybe.Extra
 
 
 existsValue : (v -> Bool) -> Dict comparable v -> Bool
 existsValue p =
-    Dict.filter (always p)
-        >> Dict.isEmpty
-        >> not
+    Dict.Extra.any (always p)
 
 
 firstSuch : (v -> Bool) -> Dict comparable v -> Maybe v
 firstSuch p =
-    Dict.filter (always p)
-        >> Dict.values
-        >> List.head
+    Dict.Extra.find (always p)
+        >> Maybe.map Tuple.second
 
 
 nameOrEmpty : Dict comparable { a | name : String } -> comparable -> String
