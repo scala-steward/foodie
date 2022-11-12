131ae92c894db619c6b918848d1fabe2c37cdeef
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-11-13 00:41:27 +0100

    Generate Elm code

---
+++ frontend/src/Api/Types/FoodInfo.elm
@@ -0,0 +1,16 @@
+module Api.Types.FoodInfo exposing (..)
+
+import Json.Decode as Decode
+import Json.Decode.Pipeline exposing (..)
+import Json.Encode as Encode
+
+
+type alias FoodInfo = { id: Int, name: String }
+
+
+decoderFoodInfo : Decode.Decoder FoodInfo
+decoderFoodInfo = Decode.succeed FoodInfo |> required "id" Decode.int |> required "name" Decode.string
+
+
+encoderFoodInfo : FoodInfo -> Encode.Value
+encoderFoodInfo obj = Encode.object [ ("id", Encode.int obj.id), ("name", Encode.string obj.name) ]
