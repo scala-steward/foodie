afadd71524e55127deee4f16bcdbd86dfec4e2c4
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-11-25 00:26:33 +0100

    Add more utility functions

--- test/services/DBTestUtil.scala
+++ test/services/DBTestUtil.scala
@@ -5,7 +5,7 @@ import play.api.db.slick.DatabaseConfigProvider
 import slick.jdbc.PostgresProfile
 import slick.jdbc.PostgresProfile.api._
 
-import scala.concurrent.Await
+import scala.concurrent.{ Await, Future }
 import scala.concurrent.duration.{ Duration, _ }
 
 object DBTestUtil {
@@ -14,18 +14,26 @@ object DBTestUtil {
   private val databaseConfigProvider = TestUtil.injector.instanceOf[DatabaseConfigProvider]
 
   def clearDb(): Unit =
-    Await.result(
-      databaseConfigProvider
-        .get[PostgresProfile]
-        .db
-        .run(
-          /* The current structure links everything to users at the
+    await(
+      dbRun(
+        /* The current structure links everything to users at the
              root level, which is why it is sufficient to delete all
              users to also clear all non-CNF tables.
-           */
-          Tables.User.delete
-        ),
-      defaultAwaitTimeout
+         */
+        Tables.User.delete
+      )
+    )
+
+  def dbRun[A](action: DBIO[A]): Future[A] =
+    databaseConfigProvider
+      .get[PostgresProfile]
+      .db
+      .run(action)
+
+  def await[A](future: Future[A], timeout: Duration = defaultAwaitTimeout): A =
+    Await.result(
+      awaitable = future,
+      atMost = timeout
     )
 
 }
