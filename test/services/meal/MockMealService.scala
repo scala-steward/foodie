448db75cb4609a595129a9d3c2849af02855574c
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-01-02 23:42:32 +0100

    Finish mock meal service

--- test/services/meal/MockMealService.scala
+++ test/services/meal/MockMealService.scala
@@ -4,6 +4,7 @@ import errors.{ ErrorContext, ServerError }
 import cats.effect.unsafe.implicits._
 import io.scalaland.chimney.dsl._
 import services.{ MealEntryId, MealId, UserId }
+import utils.TransformerUtils.Implicits._
 
 import scala.collection.mutable
 import scala.concurrent.Future
@@ -19,12 +20,12 @@ sealed trait MockMealService extends MealService {
       }
     )
 
-  private lazy val mealEntriesMap: mutable.Map[(UserId, MealEntryId), MealEntry] =
+  private lazy val mealEntriesMap: mutable.Map[(UserId, MealId, MealEntryId), MealEntry] =
     mutable.Map.from(
       fullMealsByUserId.flatMap {
         case (userId, fullMeals) =>
           fullMeals.flatMap { fullMeal =>
-            fullMeal.mealEntries.map(mealEntry => (userId, mealEntry.id) -> mealEntry)
+            fullMeal.mealEntries.map(mealEntry => (userId, fullMeal.meal.id, mealEntry.id) -> mealEntry)
           }
       }
     )
@@ -37,7 +38,12 @@ sealed trait MockMealService extends MealService {
 
   override def createMeal(userId: UserId, mealCreation: MealCreation): Future[ServerError.Or[Meal]] =
     utils.random.RandomGenerator.randomUUID
-      .map(id => Right(MealCreation.create(id.transformInto[MealId], mealCreation)))
+      .map { id =>
+        val mealId = id.transformInto[MealId]
+        val meal   = MealCreation.create(mealId, mealCreation)
+        mealMap.update((userId, mealId), meal)
+        Right(meal)
+      }
       .unsafeToFuture()
 
   override def updateMeal(userId: UserId, mealUpdate: MealUpdate): Future[ServerError.Or[Meal]] =
@@ -58,15 +64,51 @@ sealed trait MockMealService extends MealService {
         .isDefined
     }
 
-  override def getMealEntries(userId: UserId, id: MealId): Future[Seq[MealEntry]] = ???
+  override def getMealEntries(userId: UserId, id: MealId): Future[Seq[MealEntry]] =
+    Future.successful {
+      mealEntriesMap.collect { case ((uid, mid, _), mealEntry) if userId == uid && mid == id => mealEntry }.toSeq
+    }
 
   override def addMealEntry(userId: UserId, mealEntryCreation: MealEntryCreation): Future[ServerError.Or[MealEntry]] =
-    ???
+    utils.random.RandomGenerator.randomUUID
+      .map { id =>
+        val mealEntryId = id.transformInto[MealEntryId]
+        val mealEntry   = MealEntryCreation.create(mealEntryId, mealEntryCreation)
+        mealEntriesMap.update((userId, mealEntryCreation.mealId, mealEntryId), mealEntry)
+        Right(mealEntry)
+      }
+      .unsafeToFuture()
 
   override def updateMealEntry(userId: UserId, mealEntryUpdate: MealEntryUpdate): Future[ServerError.Or[MealEntry]] =
-    ???
+    Future.successful {
+      val update = for {
+        mealId <- mealEntriesMap.collectFirst {
+          case ((uid, mealId, mealEntryId), _) if uid == userId && mealEntryId == mealEntryUpdate.id => mealId
+        }
+        mealEntry <-
+          mealEntriesMap
+            .updateWith(
+              (userId, mealId, mealEntryUpdate.id)
+            )(
+              _.map(MealEntryUpdate.update(_, mealEntryUpdate))
+            )
+      } yield mealEntry
 
-  override def removeMealEntry(userId: UserId, mealEntryId: MealEntryId): Future[Boolean] = ???
+      update
+        .toRight(ErrorContext.Meal.Update(s"Unknown meal entry id: ${mealEntryUpdate.id}").asServerError)
+    }
+
+  override def removeMealEntry(userId: UserId, mealEntryId: MealEntryId): Future[Boolean] =
+    Future.successful {
+      val deletion = for {
+        mealId <- mealEntriesMap.collectFirst {
+          case ((uId, mealId, meId), _) if uId == userId && mealEntryId == meId => mealId
+        }
+        _ <- mealEntriesMap.remove((userId, mealId, mealEntryId))
+      } yield ()
+      deletion.isDefined
+    }
+
 }
 
 object MockMealService {}
