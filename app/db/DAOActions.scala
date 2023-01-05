de1a5af3baab9710ffdb6855ff9e814c1dff61b7
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-01-05 22:56:34 +0100

    Move DB implementation to instance

--- app/db/DAOActions.scala
+++ app/db/DAOActions.scala
@@ -1,6 +1,5 @@
 package db
 
-import db.DAOActions.FindPredicate
 import slick.jdbc.PostgresProfile.api._
 import slick.lifted.{ Rep, TableQuery }
 import slick.relational.RelationalProfile
@@ -9,61 +8,72 @@ import scala.concurrent.ExecutionContext
 
 trait DAOActions[Content, Table <: RelationalProfile#Table[Content], Key] {
 
-  protected def dbTable: TableQuery[Table]
-
-  protected def compareByKey: FindPredicate[Table, Key]
-
-  def find(key: Key): DBIO[Option[Content]] =
-    findQuery(key).result.headOption
+  def find(key: Key): DBIO[Option[Content]]
 
   def findBy(
       predicate: Table => Rep[Boolean]
-  ): DBIO[Seq[Content]] =
-    findPartialQuery(predicate).result
+  ): DBIO[Seq[Content]]
 
-  private def findPartialQuery(
-      predicate: Table => Rep[Boolean]
-  ): Query[Table, Content, Seq] =
-    dbTable.filter(predicate)
+  def delete(key: Key): DBIO[Int]
 
-  def delete(key: Key): DBIO[Int] =
-    findQuery(key).delete
+  def deleteBy(predicate: Table => Rep[Boolean]): DBIO[Int]
 
-  def deleteBy(predicate: Table => Rep[Boolean]): DBIO[Int] =
-    findPartialQuery(predicate).delete
+  def insert(content: Content): DBIO[Content]
 
-  def insert(
-      content: Content
-  ): DBIO[Content] =
-    dbTable.returning(dbTable) += content
+  def insertAll(contents: Seq[Content]): DBIO[Seq[Content]]
+  def update(value: Content)(keyOf: Content => Key)(implicit ec: ExecutionContext): DBIO[Boolean]
 
-  def insertAll(
-      contents: Seq[Content]
-  ): DBIO[Seq[Content]] =
-    dbTable.returning(dbTable) ++= contents
-
-  def update(value: Content)(keyOf: Content => Key)(implicit ec: ExecutionContext): DBIO[Boolean] =
-    findQuery(keyOf(value))
-      .update(value)
-      .map(_ == 1)
-
-  def exists(key: Key): DBIO[Boolean] =
-    findQuery(key).exists.result
-
-  private def findQuery(key: Key): Query[Table, Content, Seq] =
-    findPartialQuery(compareByKey(_, key))
+  def exists(key: Key): DBIO[Boolean]
 
 }
 
 object DAOActions {
-  type FindPredicate[T, Part] = (T, Part) => Rep[Boolean]
 
   abstract class Instance[Content, Table <: RelationalProfile#Table[Content], Key](
       table: TableQuery[Table],
       compare: (Table, Key) => Rep[Boolean]
   ) extends DAOActions[Content, Table, Key] {
-    override val dbTable: TableQuery[Table]              = table
-    override val compareByKey: FindPredicate[Table, Key] = (table, key) => compare(table, key)
+
+    override def find(key: Key): DBIO[Option[Content]] =
+      findQuery(key).result.headOption
+
+    override def findBy(
+        predicate: Table => Rep[Boolean]
+    ): DBIO[Seq[Content]] =
+      findPartialQuery(predicate).result
+
+    override def delete(key: Key): DBIO[Int] =
+      findQuery(key).delete
+
+    override def deleteBy(predicate: Table => Rep[Boolean]): DBIO[Int] =
+      findPartialQuery(predicate).delete
+
+    override def insert(
+        content: Content
+    ): DBIO[Content] =
+      table.returning(table) += content
+
+    override def insertAll(
+        contents: Seq[Content]
+    ): DBIO[Seq[Content]] =
+      table.returning(table) ++= contents
+
+    override def update(value: Content)(keyOf: Content => Key)(implicit ec: ExecutionContext): DBIO[Boolean] =
+      findQuery(keyOf(value))
+        .update(value)
+        .map(_ == 1)
+
+    override def exists(key: Key): DBIO[Boolean] =
+      findQuery(key).exists.result
+
+    private def findQuery(key: Key): Query[Table, Content, Seq] =
+      findPartialQuery(compare(_, key))
+
+    private def findPartialQuery(
+        predicate: Table => Rep[Boolean]
+    ): Query[Table, Content, Seq] =
+      table.filter(predicate)
+
   }
 
 }
