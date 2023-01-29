ba677a9707e582651018f6c0be6b15ba12905719
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-01-29 11:55:39 +0100

    Update migration

--- conf/db/migration/default/V9__adjust_handling_of_complex_food_units.sql
+++ conf/db/migration/default/V9__adjust_handling_of_complex_food_units.sql
@@ -5,10 +5,12 @@ alter table complex_food
     add column amount_milli_litres decimal;
 
 update complex_food
-    set amount_grams = amount where unit = 'G';
-
-update complex_food
-    set amount_milli_litres = amount where unit = 'ML';
+    set amount_milli_litres = amount_grams
+    where unit = 'ML';
 
 alter table complex_food
     drop column unit;
+
+alter table complex_food
+    drop constraint amount_positive,
+    add constraint amount_grams_positive check (amount_grams > 0);
