c18fa67f7220daf43f9afd274447c9f9b5453bfe
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-02-09 22:49:46 +0100

    Add functions that use Optionals instead of Lenses

--- frontend/src/Util/LensUtil.elm
+++ frontend/src/Util/LensUtil.elm
@@ -1,13 +1,15 @@
 module Util.LensUtil exposing (..)
 
 import Basics.Extra exposing (flip)
-
-import List.Extra
 import Monocle.Compose as Compose
 import Monocle.Lens as Lens exposing (Lens)
 import Monocle.Optional as Optional exposing (Optional)
 import Util.DictList as DictList exposing (DictList)
 import Util.Initialization as Initialization exposing (Initialization)
+
+
+
+-- todo: Tidy up functions
 
 
 dictByKey : key -> Optional (DictList key value) value
@@ -42,11 +44,22 @@ updateById id =
         >> Optional.modify
 
 
+updateByIdOptional : key -> Optional a (DictList key value) -> (value -> value) -> a -> a
+updateByIdOptional id =
+    Compose.optionalWithOptional (dictByKey id)
+        >> Optional.modify
+
+
 insertAtId : key -> Lens a (DictList key value) -> value -> a -> a
 insertAtId id lens =
-    Lens.modify lens << DictList.insert id
+    DictList.insert id >> Lens.modify lens
 
 
 deleteAtId : key -> Lens a (DictList key value) -> a -> a
 deleteAtId id lens =
-    Lens.modify lens <| DictList.remove id
+    DictList.remove id |> Lens.modify lens
+
+
+deleteAtIdOptional : key -> Optional a (DictList key value) -> a -> a
+deleteAtIdOptional id optional =
+    DictList.remove id |> Optional.modify optional
