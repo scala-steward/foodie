45200d8b2f0f4ce9007b37423a6e84cf508d4908
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-06 16:13:15 +0200

    113: Add duplication service for meals

---
+++ app/services/duplication/meal/Live.scala
@@ -0,0 +1,118 @@
+package services.duplication.meal
+
+import cats.data.OptionT
+import cats.effect.unsafe.implicits.global
+import db.generated.Tables
+import db.{ MealEntryId, MealId, UserId }
+import errors.{ ErrorContext, ServerError }
+import io.scalaland.chimney.dsl._
+import play.api.db.slick.{ DatabaseConfigProvider, HasDatabaseConfigProvider }
+import services.DBError
+import services.common.Transactionally.syntax._
+import services.meal.{ Meal, MealCreation, MealEntry, MealService }
+import slick.dbio.DBIO
+import slick.jdbc.PostgresProfile
+import slickeffect.catsio.implicits._
+import utils.TransformerUtils.Implicits._
+import utils.date.SimpleDate
+import utils.DBIOUtil.instances._
+
+import java.util.UUID
+import javax.inject.Inject
+import scala.concurrent.{ ExecutionContext, Future }
+
+class Live @Inject() (
+    override protected val dbConfigProvider: DatabaseConfigProvider,
+    companion: Duplication.Companion,
+    mealServiceCompanion: MealService.Companion
+)(implicit
+    executionContext: ExecutionContext
+) extends Duplication
+    with HasDatabaseConfigProvider[PostgresProfile] {
+
+  override def duplicate(
+      userId: UserId,
+      id: MealId
+  ): Future[ServerError.Or[Meal]] = {
+    val action = for {
+      mealEntries <- mealServiceCompanion
+        .getMealEntries(userId, Seq(id))
+        .map(_.getOrElse(id, Seq.empty))
+      newMealEntries = mealEntries.map { mealEntry =>
+        Duplication.DuplicatedMealEntry(
+          mealEntry = mealEntry,
+          newId = UUID.randomUUID().transformInto[MealEntryId]
+        )
+      }
+      newMealId = UUID.randomUUID().transformInto[MealId]
+      timestamp <- SimpleDate.now.to[DBIO]
+      newMeal <- companion.duplicateMeal(
+        userId = userId,
+        id = id,
+        newId = newMealId,
+        timestamp = timestamp
+      )
+      _ <- companion.duplicateMealEntries(newMealId, newMealEntries)
+    } yield newMeal
+
+    db.runTransactionally(action)
+      .map(Right(_))
+      .recover { case error =>
+        Left(ErrorContext.Meal.Creation(error.getMessage).asServerError)
+      }
+  }
+
+}
+
+object Live {
+
+  class Companion @Inject() (
+      mealServiceCompanion: MealService.Companion,
+      mealEntryDao: db.daos.mealEntry.DAO
+  ) extends Duplication.Companion {
+
+    override def duplicateMeal(
+        userId: UserId,
+        id: MealId,
+        newId: MealId,
+        timestamp: SimpleDate
+    )(implicit
+        ec: ExecutionContext
+    ): DBIO[Meal] = {
+      val transformer = for {
+        meal <- OptionT(mealServiceCompanion.getMeal(userId, id))
+        inserted <- OptionT.liftF(
+          mealServiceCompanion.createMeal(
+            userId = userId,
+            id = newId,
+            mealCreation = MealCreation(
+              date = timestamp,
+              name =
+                Some(s"${meal.name.fold("")(name => s"$name ")}(copy from ${SimpleDate.toPrettyString(meal.date)})")
+            )
+          )
+        )
+      } yield inserted
+
+      transformer.getOrElseF(notFound)
+    }
+
+    override def duplicateMealEntries(
+        newMealId: MealId,
+        mealEntries: Seq[Duplication.DuplicatedMealEntry]
+    )(implicit
+        ec: ExecutionContext
+    ): DBIO[Seq[MealEntry]] =
+      mealEntryDao
+        .insertAll {
+          mealEntries.map { duplicatedMealEntry =>
+            val newMealEntry = duplicatedMealEntry.mealEntry.copy(id = duplicatedMealEntry.newId)
+            (newMealEntry, newMealId).transformInto[Tables.MealEntryRow]
+          }
+        }
+        .map(_.map(_.transformInto[MealEntry]))
+
+    private def notFound[A]: DBIO[A] = DBIO.failed(DBError.Meal.NotFound)
+  }
+
+}
