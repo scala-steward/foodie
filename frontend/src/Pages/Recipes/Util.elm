d7bd7e166ef050df38c8d5a4a975911382afa758
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-13 23:56:09 +0200

    138: Extract gram parser

---
+++ frontend/src/Pages/Recipes/Util.elm
@@ -0,0 +1,17 @@
+module Pages.Recipes.Util exposing (..)
+
+import Parser exposing ((|.), Parser)
+import Result.Extra
+
+
+gramsParser : Parser Float
+gramsParser =
+    Parser.float
+        |. Parser.chompWhile ((==) ' ')
+        |. Parser.symbol "g"
+
+
+isRescalableServingSize : String -> Bool
+isRescalableServingSize =
+    Parser.run gramsParser
+        >> Result.Extra.isOk
