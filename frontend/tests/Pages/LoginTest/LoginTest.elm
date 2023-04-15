0c3b7849dbf77013b14ea191210569df213b3700
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-15 22:38:36 +0200

    Add small POC test suite

---
+++ frontend/tests/Pages/LoginTest/LoginTest.elm
@@ -0,0 +1,63 @@
+module Pages.LoginTest.LoginTest exposing (..)
+
+import Api.Types.Credentials exposing (Credentials)
+import Fuzz exposing (Fuzzer, string)
+import Html exposing (Html)
+import Pages.Login.Page
+import Pages.Login.View
+import Pages.Util.EventUtil as EventUtil
+import Pages.Util.Parameters as Parameters
+import Test exposing (..)
+import Test.Html.Event as Event
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (tag)
+
+
+credentials : Credentials
+credentials =
+    { nickname = ""
+    , password = ""
+    }
+
+
+mainModel : Pages.Login.Page.Main
+mainModel =
+    { credentials = credentials
+    }
+
+
+view : Html Pages.Login.Page.LogicMsg
+view =
+    Pages.Login.View.viewMain Parameters.configuration mainModel
+
+
+inputs : Test
+inputs =
+    describe "login"
+        [ fuzz string "set nickname" <|
+            \nickname ->
+                view
+                    |> Query.fromHtml
+                    |> Query.findAll [ tag "div", tag "input" ]
+                    |> Query.index 0
+                    |> Event.simulate (Event.input nickname)
+                    |> Event.expect (Pages.Login.Page.SetNickname nickname)
+        , fuzz string "set password" <|
+            \password ->
+                view
+                    |> Query.fromHtml
+                    |> Query.findAll [ tag "div", tag "input" ]
+                    |> Query.index 1
+                    |> Event.simulate (Event.input password)
+                    |> Event.expect (Pages.Login.Page.SetPassword password)
+        ]
+
+
+onEnterShortcut : Test
+onEnterShortcut =
+    test "Pressing enter in input" <|
+        \_ ->
+            view
+                |> Query.fromHtml
+                |> Query.findAll [ tag "div", tag "input" ]
+                |> Query.each (Event.simulate EventUtil.enter >> Event.expect Pages.Login.Page.Login)
---
+++ frontend/tests/Pages/Util/Parameters.elm
@@ -0,0 +1,10 @@
+module Pages.Util.Parameters exposing (..)
+
+import Configuration exposing (Configuration)
+
+
+configuration : Configuration
+configuration =
+    { backendURL = "localhost"
+    , mainPageURL = "localhost"
+    }
