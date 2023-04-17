4d5225ac500b7599beec20264b2ba3e0b7dbdf4a
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-18 01:16:54 +0200

    Remove unused code

--- app/utils/string/StringUtil.scala
+++ app/utils/string/StringUtil.scala
@@ -7,17 +7,10 @@ object StringUtil {
   def toBase64String(bytes: Array[Byte]): String =
     Base64.getEncoder.encodeToString(bytes)
 
-  def fromBase64String(string: String): Array[Byte] =
-    Base64.getDecoder.decode(string)
-
   object syntax {
 
     implicit class ByteArrayToBase64String(val bytes: Array[Byte]) extends AnyVal {
       def asBase64String: String = toBase64String(bytes)
-    }
-
-    implicit class StringAsBase64ByteArray(val string: String) extends AnyVal {
-      def asBase64ByteArray: Array[Byte] = fromBase64String(string)
     }
 
   }
