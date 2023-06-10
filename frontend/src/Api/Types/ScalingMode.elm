c506b75904f7d3beb1b1f8e5618551f293ca3dd8
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-06-10 14:30:32 +0200

    Regenerate Elm code

---
+++ frontend/src/Api/Types/ScalingMode.elm
@@ -0,0 +1,53 @@
+module Api.Types.ScalingMode exposing (..)
+
+import Json.Decode as Decode
+import Json.Encode as Encode
+
+
+type ScalingMode
+    = Recipe
+    | Weight
+    | Volume
+
+
+
+-- todo: Consider a more automatic approach
+
+
+decoderScalingMode : Decode.Decoder ScalingMode
+decoderScalingMode =
+    Decode.string |> Decode.andThen decoderScalingModeByString
+
+
+decoderScalingModeByString : String -> Decode.Decoder ScalingMode
+decoderScalingModeByString string =
+    case string of
+        "Recipe" ->
+            Decode.succeed Recipe
+
+        "Weight" ->
+            Decode.succeed Weight
+
+        "Volume" ->
+            Decode.succeed Volume
+
+        _ ->
+            Decode.fail ("Unexpected type for ScalingMode: " ++ string)
+
+
+encoderScalingMode : ScalingMode -> Encode.Value
+encoderScalingMode =
+    toString >> Encode.string
+
+
+toString : ScalingMode -> String
+toString scalingMode =
+    case scalingMode of
+        Recipe ->
+            "Recipe"
+
+        Weight ->
+            "Weight"
+
+        Volume ->
+            "Volume"
