42a5fb1fc11a3edf79a91a6027e3ffab8eb48910
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-02-12 21:04:36 +0100

    Regenerate type

--- frontend/src/Api/Types/MealEntryUpdate.elm
+++ frontend/src/Api/Types/MealEntryUpdate.elm
@@ -3,18 +3,17 @@ module Api.Types.MealEntryUpdate exposing (..)
 import Json.Decode as Decode
 import Json.Decode.Pipeline exposing (..)
 import Json.Encode as Encode
-import Uuid exposing (Uuid)
 
 
 type alias MealEntryUpdate =
-    { recipeId : Uuid, numberOfServings : Float }
+    { numberOfServings : Float }
 
 
 decoderMealEntryUpdate : Decode.Decoder MealEntryUpdate
 decoderMealEntryUpdate =
-    Decode.succeed MealEntryUpdate |> required "recipeId" Uuid.decoder |> required "numberOfServings" Decode.float
+    Decode.succeed MealEntryUpdate |> required "numberOfServings" Decode.float
 
 
 encoderMealEntryUpdate : MealEntryUpdate -> Encode.Value
 encoderMealEntryUpdate obj =
-    Encode.object [ ( "recipeId", Uuid.encode obj.recipeId ), ( "numberOfServings", Encode.float obj.numberOfServings ) ]
+    Encode.object [ ( "numberOfServings", Encode.float obj.numberOfServings ) ]
