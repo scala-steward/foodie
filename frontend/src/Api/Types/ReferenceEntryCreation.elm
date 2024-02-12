122a9b3e986b38e4415f0b4baa64a948ee10327b
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-02-12 21:47:28 +0100

    Regenerate front end types

--- frontend/src/Api/Types/ReferenceEntryCreation.elm
+++ frontend/src/Api/Types/ReferenceEntryCreation.elm
@@ -3,18 +3,17 @@ module Api.Types.ReferenceEntryCreation exposing (..)
 import Json.Decode as Decode
 import Json.Decode.Pipeline exposing (..)
 import Json.Encode as Encode
-import Uuid exposing (Uuid)
 
 
 type alias ReferenceEntryCreation =
-    { referenceMapId : Uuid, nutrientCode : Int, amount : Float }
+    { nutrientCode : Int, amount : Float }
 
 
 decoderReferenceEntryCreation : Decode.Decoder ReferenceEntryCreation
 decoderReferenceEntryCreation =
-    Decode.succeed ReferenceEntryCreation |> required "referenceMapId" Uuid.decoder |> required "nutrientCode" Decode.int |> required "amount" Decode.float
+    Decode.succeed ReferenceEntryCreation |> required "nutrientCode" Decode.int |> required "amount" Decode.float
 
 
 encoderReferenceEntryCreation : ReferenceEntryCreation -> Encode.Value
 encoderReferenceEntryCreation obj =
-    Encode.object [ ( "referenceMapId", Uuid.encode obj.referenceMapId ), ( "nutrientCode", Encode.int obj.nutrientCode ), ( "amount", Encode.float obj.amount ) ]
+    Encode.object [ ( "nutrientCode", Encode.int obj.nutrientCode ), ( "amount", Encode.float obj.amount ) ]
