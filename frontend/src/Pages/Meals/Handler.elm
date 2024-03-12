3c571c59dd7eaedeabef6af757ac0396a398e58f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-03-12 22:20:55 +0100

    Account for profile id

--- frontend/src/Pages/Meals/Handler.elm
+++ frontend/src/Pages/Meals/Handler.elm
@@ -1,6 +1,7 @@
 module Pages.Meals.Handler exposing (init, update)
 
 import Addresses.Frontend
+import Api.Auxiliary exposing (ProfileId)
 import Api.Types.Meal exposing (Meal)
 import Maybe.Extra
 import Pages.Meals.MealCreationClientInput as MealCreationClientInput exposing (MealCreationClientInput)
@@ -17,25 +18,35 @@ import Pages.View.Tristate as Tristate
 
 init : Page.Flags -> ( Page.Model, Cmd Page.Msg )
 init flags =
-    ( Pages.Util.ParentEditor.Page.initial flags.authorizedAccess
-    , Requests.fetchMeals flags.authorizedAccess |> Cmd.map Tristate.Logic
+    ( { parentEditor = Pages.Util.ParentEditor.Page.initial flags.authorizedAccess
+      , profileId = flags.profileId
+      }
+    , Requests.fetchMeals flags.authorizedAccess flags.profileId |> Cmd.map Tristate.Logic
     )
 
 
 update : Page.Msg -> Page.Model -> ( Page.Model, Cmd Page.Msg )
-update =
-    Tristate.updateWith updateLogic
+update msg model =
+    let
+        ( updatedParentEditor, parentEditorCmd ) =
+            Tristate.updateWith (updateLogic model.profileId) msg model.parentEditor
+    in
+    ( { parentEditor = updatedParentEditor
+      , profileId = model.profileId
+      }
+    , parentEditorCmd
+    )
 
 
-updateLogic : Page.LogicMsg -> Page.Model -> ( Page.Model, Cmd Page.LogicMsg )
-updateLogic =
+updateLogic : ProfileId -> Page.LogicMsg -> Tristate.Model Page.Main Page.Initial -> ( Tristate.Model Page.Main Page.Initial, Cmd Page.LogicMsg )
+updateLogic profileId =
     Pages.Util.ParentEditor.Handler.updateLogic
         { idOfParent = .id
         , toUpdate = MealUpdateClientInput.from
-        , navigateToAddress = Addresses.Frontend.mealEntryEditor.address
+        , navigateToAddress = \mealId -> Addresses.Frontend.mealEntryEditor.address ( profileId, mealId )
         , updateCreationTimestamp = DateUtil.fromPosix >> SimpleDateInput.from >> MealCreationClientInput.lenses.date.set
-        , create = \authorizedAccess -> MealCreationClientInput.toCreation >> Maybe.Extra.unwrap Cmd.none (Requests.createMeal authorizedAccess)
-        , save = \authorizedAccess mealId -> MealUpdateClientInput.to >> Maybe.Extra.unwrap Cmd.none (Requests.saveMeal authorizedAccess mealId)
-        , delete = Requests.deleteMeal
-        , duplicate = Pages.Util.Requests.duplicateMealWith Pages.Util.ParentEditor.Page.GotDuplicateResponse
+        , create = \authorizedAccess -> MealCreationClientInput.toCreation >> Maybe.Extra.unwrap Cmd.none (Requests.createMeal authorizedAccess profileId)
+        , save = \authorizedAccess mealId -> MealUpdateClientInput.to >> Maybe.Extra.unwrap Cmd.none (Requests.saveMeal authorizedAccess profileId mealId)
+        , delete = \authorizedAccess -> Requests.deleteMeal authorizedAccess profileId
+        , duplicate = \authorizedAccess -> Pages.Util.Requests.duplicateMealWith Pages.Util.ParentEditor.Page.GotDuplicateResponse authorizedAccess profileId
         }
