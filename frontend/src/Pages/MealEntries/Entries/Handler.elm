dbf82a827739102684977e39105d644480ff79cb
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-03-24 23:11:12 +0100

    Account for profile

--- frontend/src/Pages/MealEntries/Entries/Handler.elm
+++ frontend/src/Pages/MealEntries/Entries/Handler.elm
@@ -1,33 +1,66 @@
 module Pages.MealEntries.Entries.Handler exposing (..)
 
-import Api.Auxiliary exposing (MealId)
+import Api.Auxiliary exposing (MealId, ProfileId)
+import Maybe.Extra
 import Pages.MealEntries.Entries.Page as Page
 import Pages.MealEntries.Entries.Requests as Requests
 import Pages.MealEntries.MealEntryCreationClientInput as MealEntryCreationClientInput
 import Pages.MealEntries.MealEntryUpdateClientInput as MealEntryUpdateClientInput
 import Pages.Util.AuthorizedAccess exposing (AuthorizedAccess)
 import Pages.Util.Choice.Handler
+import Pages.Util.Requests
+import Pages.View.Tristate as Tristate
+import Pages.View.TristateUtil as TristateUtil
+import Result.Extra
 
 
-initialFetch : AuthorizedAccess -> MealId -> Cmd Page.LogicMsg
-initialFetch authorizedAccess mealId =
+initialFetch : AuthorizedAccess -> ProfileId -> MealId -> Cmd Page.LogicMsg
+initialFetch authorizedAccess profileId mealId =
     Cmd.batch
         [ Requests.fetchRecipes authorizedAccess
-        , Requests.fetchMealEntries authorizedAccess mealId
+        , Requests.fetchMealEntries authorizedAccess profileId mealId
+        , Pages.Util.Requests.fetchProfileWith Page.GotFetchProfileResponse authorizedAccess profileId
         ]
 
 
 updateLogic : Page.LogicMsg -> Page.Model -> ( Page.Model, Cmd Page.LogicMsg )
-updateLogic =
-    Pages.Util.Choice.Handler.updateLogic
-        { idOfElement = .id
-        , idOfChoice = .id
-        , choiceIdOfElement = .recipeId
-        , choiceIdOfCreation = .recipeId
-        , toUpdate = MealEntryUpdateClientInput.from
-        , toCreation = \recipe -> MealEntryCreationClientInput.default recipe.id
-        , createElement = \authorizedAccess mealId creationInput -> MealEntryCreationClientInput.toCreation creationInput |> Requests.createMealEntry authorizedAccess mealId
-        , saveElement = \authorizedAccess mealId mealEntryId updateInput -> MealEntryUpdateClientInput.to updateInput |> Requests.saveMealEntry authorizedAccess mealId mealEntryId
-        , deleteElement = Requests.deleteMealEntry
-        , storeChoices = \_ -> Cmd.none
-        }
+updateLogic msg model =
+    case msg of
+        Page.ChoiceMsg choiceMsg ->
+            TristateUtil.updateFromSubModel
+                { initialSubModelLens = Page.lenses.initial.choices
+                , mainSubModelLens = Page.lenses.main.choices
+                , fromInitToMain = Page.initialToMain
+                , updateSubModel =
+                    \subModelMsg subModel ->
+                        model
+                            |> Page.profileId
+                            |> Maybe.Extra.unwrap ( subModel, Cmd.none )
+                                (\profileId ->
+                                    Pages.Util.Choice.Handler.updateLogic
+                                        { idOfElement = .id
+                                        , idOfChoice = .id
+                                        , choiceIdOfElement = .recipeId
+                                        , choiceIdOfCreation = .recipeId
+                                        , toUpdate = MealEntryUpdateClientInput.from
+                                        , toCreation = \recipe -> MealEntryCreationClientInput.default recipe.id
+                                        , createElement = \authorizedAccess mealId creationInput -> MealEntryCreationClientInput.toCreation creationInput |> Requests.createMealEntry authorizedAccess profileId mealId
+                                        , saveElement = \authorizedAccess mealId mealEntryId updateInput -> MealEntryUpdateClientInput.to updateInput |> Requests.saveMealEntry authorizedAccess profileId mealId mealEntryId
+                                        , deleteElement = \authorizedAccess -> Requests.deleteMealEntry authorizedAccess profileId
+                                        , storeChoices = \_ -> Cmd.none
+                                        }
+                                        subModelMsg
+                                        subModel
+                                )
+                , toMsg = Page.ChoiceMsg
+                }
+                choiceMsg
+                model
+
+        Page.GotFetchProfileResponse result ->
+            ( result
+                |> Result.Extra.unpack
+                    (Tristate.toError model)
+                    (\profile -> Tristate.mapInitial (Page.lenses.initial.profile.set (Just profile)) model)
+            , Cmd.none
+            )
--- frontend/src/Pages/MealEntries/Entries/Page.elm
+++ frontend/src/Pages/MealEntries/Entries/Page.elm
@@ -1,12 +1,15 @@
 module Pages.MealEntries.Entries.Page exposing (..)
 
-import Api.Auxiliary exposing (JWT, MealEntryId, MealId, RecipeId)
+import Api.Auxiliary exposing (JWT, MealEntryId, MealId, ProfileId, RecipeId)
 import Api.Types.MealEntry exposing (MealEntry)
+import Api.Types.Profile exposing (Profile)
 import Api.Types.Recipe exposing (Recipe)
+import Monocle.Lens exposing (Lens)
 import Pages.MealEntries.MealEntryCreationClientInput exposing (MealEntryCreationClientInput)
 import Pages.MealEntries.MealEntryUpdateClientInput exposing (MealEntryUpdateClientInput)
 import Pages.Util.Choice.Page
 import Pages.View.Tristate as Tristate
+import Util.HttpUtil exposing (Error)
 
 
 type alias Model =
@@ -14,12 +17,64 @@ type alias Model =
 
 
 type alias Initial =
-    Pages.Util.Choice.Page.Initial MealId MealEntryId MealEntry RecipeId Recipe
+    { choices : Pages.Util.Choice.Page.Initial MealId MealEntryId MealEntry RecipeId Recipe
+    , profile : Maybe Profile
+    }
 
 
 type alias Main =
-    Pages.Util.Choice.Page.Main MealId MealEntryId MealEntry MealEntryUpdateClientInput RecipeId Recipe MealEntryCreationClientInput
+    { choices : Pages.Util.Choice.Page.Main MealId MealEntryId MealEntry MealEntryUpdateClientInput RecipeId Recipe MealEntryCreationClientInput
+    , profile : Profile
+    }
 
 
-type alias LogicMsg =
+lenses :
+    { initial :
+        { choices : Lens Initial (Pages.Util.Choice.Page.Initial MealId MealEntryId MealEntry RecipeId Recipe)
+        , profile : Lens Initial (Maybe Profile)
+        }
+    , main :
+        { choices : Lens Main (Pages.Util.Choice.Page.Main MealId MealEntryId MealEntry MealEntryUpdateClientInput RecipeId Recipe MealEntryCreationClientInput)
+        , profile : Lens Main Profile
+        }
+    }
+lenses =
+    { initial =
+        { choices = Lens .choices (\b a -> { a | choices = b })
+        , profile = Lens .profile (\b a -> { a | profile = b })
+        }
+    , main =
+        { choices = Lens .choices (\b a -> { a | choices = b })
+        , profile = Lens .profile (\b a -> { a | profile = b })
+        }
+    }
+
+
+initialToMain : Initial -> Maybe Main
+initialToMain initial =
+    Maybe.map2
+        (\choices profile ->
+            { choices = choices
+            , profile = profile
+            }
+        )
+        (initial.choices |> Pages.Util.Choice.Page.initialToMain)
+        initial.profile
+
+
+profileId : Model -> Maybe ProfileId
+profileId =
+    Tristate.fold
+        { onInitial = .profile >> Maybe.map .id
+        , onMain = .profile >> .id >> Just
+        , onError = always Nothing
+        }
+
+
+type alias ChoiceLogicMsg =
     Pages.Util.Choice.Page.LogicMsg MealEntryId MealEntry MealEntryUpdateClientInput RecipeId Recipe MealEntryCreationClientInput
+
+
+type LogicMsg
+    = ChoiceMsg ChoiceLogicMsg
+    | GotFetchProfileResponse (Result Error Profile)
