2c8bff8ae7e7b8695ae15ad8b777d1e9fce933cc
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-03-24 16:50:12 +0100

    Adjust page and handler in the same fashion as for the parent editor

--- frontend/src/Pages/MealEntries/Meal/Handler.elm
+++ frontend/src/Pages/MealEntries/Meal/Handler.elm
@@ -1,36 +1,72 @@
 module Pages.MealEntries.Meal.Handler exposing (..)
 
 import Addresses.Frontend
-import Api.Auxiliary exposing (MealId)
+import Api.Auxiliary exposing (MealId, ProfileId)
+import Maybe.Extra
 import Pages.MealEntries.Meal.Page as Page
 import Pages.Meals.MealUpdateClientInput as MealUpdateClientInput
 import Pages.Util.AuthorizedAccess exposing (AuthorizedAccess)
 import Pages.Util.Parent.Handler
 import Pages.Util.Parent.Page
 import Pages.Util.Requests
+import Pages.View.Tristate as Tristate
+import Pages.View.TristateUtil as TristateUtil
+import Result.Extra
 
 
-initialFetch : AuthorizedAccess -> MealId -> Cmd Page.LogicMsg
-initialFetch =
-    Pages.Util.Requests.fetchMealWith Pages.Util.Parent.Page.GotFetchResponse
+initialFetch : AuthorizedAccess -> ProfileId -> MealId -> Cmd Page.LogicMsg
+initialFetch authorizedAccess profileId mealId =
+    Cmd.batch
+        [ Pages.Util.Requests.fetchMealWith Pages.Util.Parent.Page.GotFetchResponse authorizedAccess profileId mealId
+            |> Cmd.map Page.ParentMsg
+        , Pages.Util.Requests.fetchProfileWith Page.GotFetchProfileResponse authorizedAccess profileId
+        ]
 
 
 updateLogic : Page.LogicMsg -> Page.Model -> ( Page.Model, Cmd Page.LogicMsg )
-updateLogic =
-    Pages.Util.Parent.Handler.updateLogic
-        { toUpdate = MealUpdateClientInput.from
-        , idOf = .id
-        , save =
-            \authorizedAccess mealId ->
-                MealUpdateClientInput.to
-                    >> Maybe.map
-                        (Pages.Util.Requests.saveMealWith
-                            Pages.Util.Parent.Page.GotSaveEditResponse
-                            authorizedAccess
-                            mealId
-                        )
-        , delete = Pages.Util.Requests.deleteMealWith Pages.Util.Parent.Page.GotDeleteResponse
-        , duplicate = Pages.Util.Requests.duplicateMealWith Pages.Util.Parent.Page.GotDuplicateResponse
-        , navigateAfterDeletionAddress = Addresses.Frontend.meals.address
-        , navigateAfterDuplicationAddress = Addresses.Frontend.mealEntryEditor.address
-        }
+updateLogic msg model =
+    case msg of
+        Page.ParentMsg parentMsg ->
+            TristateUtil.updateFromSubModel
+                { initialSubModelLens = Page.lenses.initial.parent
+                , mainSubModelLens = Page.lenses.main.parent
+                , fromInitToMain = Page.initialToMain
+                , updateSubModel =
+                    \subModelMsg subModel ->
+                        model
+                            |> Page.profileId
+                            |> Maybe.Extra.unwrap ( subModel, Cmd.none )
+                                (\profileId ->
+                                    Pages.Util.Parent.Handler.updateLogic
+                                        { toUpdate = MealUpdateClientInput.from
+                                        , idOf = .id
+                                        , save =
+                                            \authorizedAccess mealId ->
+                                                MealUpdateClientInput.to
+                                                    >> Maybe.map
+                                                        (Pages.Util.Requests.saveMealWith
+                                                            Pages.Util.Parent.Page.GotSaveEditResponse
+                                                            authorizedAccess
+                                                            profileId
+                                                            mealId
+                                                        )
+                                        , delete = \authorizedAccess -> Pages.Util.Requests.deleteMealWith Pages.Util.Parent.Page.GotDeleteResponse authorizedAccess profileId
+                                        , duplicate = \authorizedAccess -> Pages.Util.Requests.duplicateMealWith Pages.Util.Parent.Page.GotDuplicateResponse authorizedAccess profileId
+                                        , navigateAfterDeletionAddress = \_ -> Addresses.Frontend.meals.address profileId
+                                        , navigateAfterDuplicationAddress = \mealId -> Addresses.Frontend.mealEntryEditor.address ( profileId, mealId )
+                                        }
+                                        subModelMsg
+                                        subModel
+                                )
+                , toMsg = Page.ParentMsg
+                }
+                parentMsg
+                model
+
+        Page.GotFetchProfileResponse result ->
+            ( result
+                |> Result.Extra.unpack
+                    (Tristate.toError model)
+                    (\profile -> Tristate.mapInitial (Page.lenses.initial.profile.set (Just profile)) model)
+            , Cmd.none
+            )
--- frontend/src/Pages/MealEntries/Meal/Page.elm
+++ frontend/src/Pages/MealEntries/Meal/Page.elm
@@ -1,9 +1,13 @@
 module Pages.MealEntries.Meal.Page exposing (..)
 
+import Api.Auxiliary exposing (ProfileId)
 import Api.Types.Meal exposing (Meal)
+import Api.Types.Profile exposing (Profile)
+import Monocle.Lens exposing (Lens)
 import Pages.Meals.MealUpdateClientInput exposing (MealUpdateClientInput)
 import Pages.Util.Parent.Page
 import Pages.View.Tristate as Tristate
+import Util.HttpUtil exposing (Error)
 
 
 type alias Model =
@@ -11,12 +15,60 @@ type alias Model =
 
 
 type alias Main =
-    Pages.Util.Parent.Page.Main Meal MealUpdateClientInput
+    { parent : Pages.Util.Parent.Page.Main Meal MealUpdateClientInput
+    , profile : Profile
+    }
 
 
 type alias Initial =
-    Pages.Util.Parent.Page.Initial Meal
+    { parent : Pages.Util.Parent.Page.Initial Meal
+    , profile : Maybe Profile
+    }
 
 
-type alias LogicMsg =
-    Pages.Util.Parent.Page.LogicMsg Meal MealUpdateClientInput
+lenses :
+    { initial :
+        { parent : Lens Initial (Pages.Util.Parent.Page.Initial Meal)
+        , profile : Lens Initial (Maybe Profile)
+        }
+    , main :
+        { parent : Lens Main (Pages.Util.Parent.Page.Main Meal MealUpdateClientInput)
+        , profile : Lens Main Profile
+        }
+    }
+lenses =
+    { initial =
+        { parent = Lens .parent (\b a -> { a | parent = b })
+        , profile = Lens .profile (\b a -> { a | profile = b })
+        }
+    , main =
+        { parent = Lens .parent (\b a -> { a | parent = b })
+        , profile = Lens .profile (\b a -> { a | profile = b })
+        }
+    }
+
+
+initialToMain : Initial -> Maybe Main
+initialToMain initial =
+    Maybe.map2
+        (\parent profile ->
+            { parent = parent
+            , profile = profile
+            }
+        )
+        (initial.parent |> Pages.Util.Parent.Page.initialToMain)
+        initial.profile
+
+
+profileId : Model -> Maybe ProfileId
+profileId =
+    Tristate.fold
+        { onInitial = .profile >> Maybe.map .id
+        , onMain = .profile >> .id >> Just
+        , onError = always Nothing
+        }
+
+
+type LogicMsg
+    = ParentMsg (Pages.Util.Parent.Page.LogicMsg Meal MealUpdateClientInput)
+    | GotFetchProfileResponse (Result Error Profile)
